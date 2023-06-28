c26098b609fcefc2049a013bcfebcd954cbfa53d
Ambuj Singh <ambujs89@gmail.com>
2023-06-28 17:23:15 +0530

    Initial Commit

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
