2ab03be28b63d28702b4b5f775e04672a5172d26
Ambuj Singh <ambujs89@gmail.com>
2023-06-29 13:59:23 +0530

    feat: connect mongoDB and configure error handler

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,15 +1,49 @@
-import express from 'express';
+import express, { ErrorRequestHandler } from "express";
+import { config } from "dotenv";
+import { connect } from "mongoose";
+import nocache from "nocache";
 
-const host = process.env.HOST ?? 'localhost';
+import router from "./routers";
+
+config();
+
+const host = process.env.HOST ?? "localhost";
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = express();
+import "express-async-errors";
 
+connect(process.env.MONGO_URI)
+  .then(() => {
+    console.info("Successfully connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(`Error occured while connecting to MongoDB: ${err}`);
+  });
+
+const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
+app.set("etag", false);
+app.use(nocache());
+
+app.use("/api", router);
+
+function errorHandler(): ErrorRequestHandler {
+  return (err, _, res, next) => {
+    if (res.headersSent) {
+      return next(err);
+    }
+
+    let status = err.statusCode ?? err.status ?? 500;
+    const error = err.message ?? "Internal Server Error";
+
+    if (err.name === "ValidationError") status = 400;
+
+    res.status(status).json({ status, error });
+  };
+}
+
+app.use(errorHandler());
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
