d6c6b0978ecc6582b367820b7156b7d5b3856f17
Ambuj Singh <ambujs89@gmail.com>
2023-07-01 11:15:22 +0530

    feat(frontend): add setEnvironment() script for axios in dev mode

---
+++ apps/frontend/app/environment.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const isDevMode = process.env.NODE_ENV === "development";
+
+export function setEnvironment() {
+  if (isDevMode) {
+    axios.defaults.baseURL = "http://localhost:3000";
+  }
+}
