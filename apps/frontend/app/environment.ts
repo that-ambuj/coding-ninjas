5f1809f8831a0a853767057467f7a81f5c599dce
Ambuj Singh <ambujs89@gmail.com>
2023-07-03 18:42:53 +0530

    chore(frontent): set Hostname to environment variable HOST for nextjs

--- apps/frontend/app/environment.ts
+++ apps/frontend/app/environment.ts
@@ -5,5 +5,7 @@ export const isDevMode = process.env.NODE_ENV === "development";
 export function setEnvironment() {
   if (isDevMode) {
     axios.defaults.baseURL = "http://localhost:3000";
+  } else {
+    axios.defaults.baseURL = process.env.HOST;
   }
 }
